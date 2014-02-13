ba505c45542077409b86ad9258dbf1ea46ce9eb3
Paul Meskers <paulmeskers@gmail.com>
2014-02-13 17:15:42 -0500

    Run build when starting server

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,5 +61,5 @@ module.exports = function(grunt) {
 
   grunt.registerTask('build', ['compass', 'uglify', 'copy']);
   grunt.registerTask('default', ['jshint', 'build']);
-  grunt.registerTask('server', ['express:dev', 'watch']);
+  grunt.registerTask('server', ['build', 'express:dev', 'watch']);
 };
